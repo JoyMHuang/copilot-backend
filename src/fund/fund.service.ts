b895c46b776ec3147bf03b2240305b0472d79062
joyhuang <joy_m_huang@manulife.com>
2025-08-01 15:05:46 +0800

    init

--- src/fund/fund.service.ts
+++ src/fund/fund.service.ts
@@ -1,17 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { mockFunds, mockFundPerformances } from '../mock-data';
 
 @Injectable()
 export class FundService {
   findAll() {
-    return mockFunds;
-  }
-
-  findOne(id: string) {
-    return mockFunds.find(fund => fund.id === id);
-  }
-
-  getFundPerformance(id: string) {
-    return mockFundPerformances.filter(perf => perf.fundId === id);
+    return [];
   }
 }
--- src/mock-data/interfaces/index.ts
+++ src/mock-data/interfaces/index.ts
@@ -1,4 +1,3 @@
 // 导出所有接口
 export * from './customer.interface';
-export * from './fund.interface';
 export * from './transaction.interface';
