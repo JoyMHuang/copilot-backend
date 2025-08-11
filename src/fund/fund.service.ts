7c7c0b9496426fb03b83f41647ff7a71b7747005
ZhenYu Zhang <Cartman_Z_Zhang@manulife.com>
2025-08-11 11:04:08 +0800

    feat: 1

--- src/fund/fund.service.ts
+++ src/fund/fund.service.ts
@@ -1,25 +1,13 @@
 import { Injectable } from '@nestjs/common';
-<<<<<<< Updated upstream
-=======
-import { mockFunds, mockFundPerformances } from '../mock-data';
 import { fundMockData } from './fund.mock';
->>>>>>> Stashed changes
 
 @Injectable()
 export class FundService {
   findAll() {
-<<<<<<< Updated upstream
-    return [];
-=======
     return fundMockData;
   }
 
-  findOne(id: string) {
-    return mockFunds.find(fund => fund.id === id);
-  }
-
-  getFundPerformance(id: string) {
-    return mockFundPerformances.filter(perf => perf.fundId === id);
->>>>>>> Stashed changes
+  findOne(id: number) {
+    return fundMockData.find(fund => fund.id === id);
   }
 }
