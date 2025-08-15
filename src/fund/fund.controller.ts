1b2a9aaa717eb590078009b6203fc85d80ff5638
tonyli <tony_z_li@manulife.com>
2025-08-15 15:45:06 +0800

    feat: fix the issue

--- src/fund/fund.controller.ts
+++ src/fund/fund.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { FundService } from './fund.service';
 import { FundDto } from './dto/fund.dto';
